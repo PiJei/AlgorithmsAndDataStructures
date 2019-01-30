4e9803a9be01e0b19c7c5fb16e91ea0a0125ac12
Parisa <parisa.j@gmail.com>
2019-01-30 20:08:26 +0000

    adding the commons lib for sorts

---
+++ CSFundamentalAlgorithms/SortingAlgs/SortingCommon.cs
@@ -0,0 +1,33 @@
+﻿/* 
+ * Copyright (c) 2019 (PiJei) 
+ * 
+ * This file is part of CSFundamentalAlgorithms project.
+ *
+ * CSFundamentalAlgorithms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CSFundamentalAlgorithms is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CSFundamentalAlgorithms.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Collections.Generic;
+
+namespace CSFundamentalAlgorithms.SortingAlgs
+{
+    public class SortingCommon
+    {
+        public static void Swap(List<int> values, int index1, int index2)
+        {
+            int temp = values[index1];
+            values[index1] = values[index2];
+            values[index2] = temp;
+        }
+    }
+}
