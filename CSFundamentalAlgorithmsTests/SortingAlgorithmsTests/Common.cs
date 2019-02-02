0a5163db0e57e8f2865c6343fadc0f9ce7a7ccd5
Parisa <parisa.j@gmail.com>
2019-02-02 17:33:42 +0000

    added some read only arrays for testing other scenarios of sort algorithms

--- CSFundamentalAlgorithmsTests/SortingAlgorithmsTests/Common.cs
+++ CSFundamentalAlgorithmsTests/SortingAlgorithmsTests/Common.cs
@@ -24,12 +24,36 @@ namespace CSFundamentalAlgorithmsTests.SortingAlgorithmsTests
 {
     public class Common
     {
-        public static List<int> ArrayWithDistinctValues = new List<int> { 100, 2, 3, 1, 56, 78, 209, 46, 21, 10, 12, 15, 51 };
+        /// <summary>
+        /// Is an array of integers with only distinct values. 
+        /// </summary>
+        public static readonly List<int> ArrayWithDistinctValues = new List<int> { 100, 2, 3, 1, 56, 78, 209, 46, 21, 10, 12, 15, 51 };
 
-        public static List<int> ArrayWithDuplicateValues = new List<int> { 100, 2, 3, 1, 56, 78, 209, 46, 78, 10, 12, 1, 51, 15 };
+        /// <summary>
+        /// Is an array of integers with duplicate values. 
+        /// </summary>
+        public static List<int> ArrayWithDuplicateValues = new List<int> { 100, 2, 3, 1, 56, 78, 209, 21, 46, 78, 10, 12, 1, 51, 15 };
 
-        // TODO: Add arrays already sorted. 
-        // TODO: Add arrays sorted reversly
+        /// <summary>
+        /// Is an array of integers with distinct values, such that the array is already sorted ascending. 
+        /// </summary>
+        public static List<int> ArrayWithSortedDistinctValues = new List<int> { 1, 2, 3, 10, 12, 15, 21, 46, 51, 56, 78, 100, 209 };
+
+        /// <summary>
+        /// Is an array of integers with duplicate values, such that the array is already sorted ascending. 
+        /// </summary>
+        public static List<int> ArrayWithSortedDuplicateValues = new List<int> { 1, 2, 3, 10, 12, 15, 21, 21, 46, 51, 56, 78, 100, 209 };
+
+        /// <summary>
+        /// Is an array of integers with distinct values, such that the array is reversly sorted, meaning it is descending, whereas sort meant ascending.  
+        /// </summary>
+        public static List<int> ArrayWithReverselySortedDistinctValues = new List<int> { 209, 100, 78, 56, 51, 46, 21, 15, 12, 10, 3, 2, 1 };
+
+        /// <summary>
+        /// Is an array of integers with duplicate values, such that the array is reversly sorted, meaning it is descending, whereas sort meant ascending. 
+        /// </summary>
+        public static List<int> ArrayWithReverselySortedDuplicateValues = new List<int> { 209, 100, 78, 56, 51, 46, 21, 21, 15, 12, 10, 3, 2, 1, 1 };
+
         // TODO: Measure the timing for each case, ... 
 
         /// <summary>
