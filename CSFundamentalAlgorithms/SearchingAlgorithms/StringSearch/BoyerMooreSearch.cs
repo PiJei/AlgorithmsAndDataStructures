fe403c14229ba708fc0be0e176269e242d4c8a1a
Parisa <parisa.j@gmail.com>
2019-02-20 09:58:20 +0000

    added a method for mapping all the characters in a string to the last index they occure at

--- CSFundamentalAlgorithms/SearchingAlgorithms/StringSearch/BoyerMooreSearch.cs
+++ CSFundamentalAlgorithms/SearchingAlgorithms/StringSearch/BoyerMooreSearch.cs
@@ -17,9 +17,34 @@
  * along with CSFundamentalAlgorithms.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+using System.Collections.Generic;
+
 namespace CSFundamentalAlgorithms.SearchingAlgorithms.StringSearch
 {
     public class BoyerMooreSearch
     {
+
+        /// <summary>
+        /// Maps every character in the given string to its last index in the string. 
+        /// An example use is for bad character lookup in Boyer-Moore search algorithm.
+        /// </summary>
+        /// <returns>A mapping of all the characters in the given string to their last index in the string. </returns>
+        public static Dictionary<char, int> MapCharToLastIndex(string text)
+        {
+            Dictionary<char, int> indexes = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (indexes.ContainsKey(text[i]))
+                {
+                    indexes[text[i]] = i;
+                }
+                else
+                {
+                    indexes.Add(text[i], i);
+                }
+            }
+            return indexes;
+        }
     }
 }
--- CSFundamentalAlgorithmsTests/SearchingAlgorithmsTests/StringSearchTests/BoyerMooreSearchTests.cs
+++ CSFundamentalAlgorithmsTests/SearchingAlgorithmsTests/StringSearchTests/BoyerMooreSearchTests.cs
@@ -18,12 +18,24 @@
  */
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSFundamentalAlgorithms.SearchingAlgorithms.StringSearch;
+using System.Collections.Generic;
 
 namespace CSFundamentalAlgorithmsTests.SearchingAlgorithmsTests.StringSearchTests
 {
     [TestClass]
     public class BoyerMooreSearchTests
     {
-
+        [TestMethod]
+        public void BoyerMooreSearch_MapCharToLastIndex_Test()
+        {
+            Dictionary<char, int> map1 = BoyerMooreSearch.MapCharToLastIndex("aabcdakka");
+            Assert.AreEqual(5, map1.Keys.Count);
+            Assert.AreEqual(8, map1['a']);
+            Assert.AreEqual(2, map1['b']);
+            Assert.AreEqual(3, map1['c']);
+            Assert.AreEqual(7, map1['k']);
+            Assert.AreEqual(4, map1['d']);
+        }
     }
 }
