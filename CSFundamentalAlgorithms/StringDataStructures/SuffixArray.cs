9f2820187705f902ea352e9427db8130e310c7b2
Parisa <parisa.j@gmail.com>
2019-02-25 09:20:20 +0000

    added a data structure for storing a suffix

--- CSFundamentalAlgorithms/StringDataStructures/SuffixArray.cs
+++ CSFundamentalAlgorithms/StringDataStructures/SuffixArray.cs
@@ -34,8 +34,41 @@ namespace CSFundamentalAlgorithms.StringDataStructures
         {
             List<int> suffixArray = new List<int>(); // length is text.Length
 
+            List<StringSuffix> suffixes = new List<StringSuffix>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char firstChar = text[i];
+                char secondChar = (i + 1) < text.Length ? text[i + 1] : '\0';
+                suffixes.Add(new StringSuffix(i, firstChar, secondChar));
+            }
 
             return suffixArray;
+        }
+    }
+
+    /// <summary>
+    /// Stores information about a suffix of a string. 
+    /// </summary>
+    public class StringSuffix
+    {
+        /// <summary>
+        /// Specifies the 0-based starting index of this suffix in the string
+        /// Notice that there is no need to store the end index, as it is always string.Length - 1, based on the suffix definition. 
+        /// </summary>
+        public int StartIndex { get; set; }
+
+        /// <summary>
+        /// Specifies the rank for (starting char, second char) of the suffix. If second char does not exist, -1 rather than rank. 
+        /// Rank of a character is computing using: 'char'-'a'. 
+        /// </summary>
+        public int[] RankPair { get; set; } = new int[2];
+
+        /// <param name="secondChar">If -1, means there is no second char. </param>
+        public StringSuffix(int startIndex, char firstChar, char secondChar)
+        {
+            StartIndex = startIndex;
+            RankPair[0] = firstChar - 'a';
+            RankPair[1] = secondChar != '\0' ? secondChar - 'a' : -1;
         }
     }
 }
