f0120c736d90fb122af1f89e42272ca269c5a4b8
Parisa <parisa.j@gmail.com>
2019-02-25 07:58:53 +0000

    added place holders for the source and test code of suffix array implementation

---
+++ CSFundamentalAlgorithms/SuffixArray.cs
@@ -0,0 +1,30 @@
+﻿/* 
+ * Copyright (c) 2019 (PiJei) 
+ * 
+ * This file is part of CSFundamentalAlgorithms project.
+ *
+ * CSFundamentalAlgorithms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CSFundamentalAlgorithms is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CSFundamentalAlgorithms.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace CSFundamentalAlgorithms
+{
+    /// <summary>
+    /// Implements SuffixArray data structure. A suffix array of an string is an array of integers that contains the starting index of all suffixes of the string in alphabetically sorted order. 
+    /// For example for string 'data' , where suffixes are : 'data', 'ata', 'ta', 'a' the suffix array is [3, 1, 0, 2]
+    /// </summary>
+    public class SuffixArray
+    {
+
+    }
+}
