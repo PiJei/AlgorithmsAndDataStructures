4d6888b8a1f292e21ed8e1ebac815742005073a5
PiJei <parisa.j@gmail.com>
2019-03-06 20:11:20 +0000

    made redblack tree to implement tree

--- Source/DataStructures/Trees/RedBlackTree.cs
+++ Source/DataStructures/Trees/RedBlackTree.cs
@@ -17,9 +17,36 @@
  * along with CSFundamentals.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+using System;
+using System.Collections.Generic;
+
 namespace CSFundamentals.DataStructures.Trees
 {
-    public class RedBlackTree
+    public class RedBlackTree<T1, T2> : Tree<T1, T2> where T1 : IComparable<T1>, IEquatable<T1>
     {
+        public override BinaryTreeNode<T1, T2> Build(Dictionary<T1, T2> keyValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BinaryTreeNode<T1, T2> Delete(BinaryTreeNode<T1, T2> root, T1 key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BinaryTreeNode<T1, T2> Insert(BinaryTreeNode<T1, T2> root, T1 key, T2 value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BinaryTreeNode<T1, T2> Search(BinaryTreeNode<T1, T2> root, T1 key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Update(BinaryTreeNode<T1, T2> root, T1 key, T2 value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
