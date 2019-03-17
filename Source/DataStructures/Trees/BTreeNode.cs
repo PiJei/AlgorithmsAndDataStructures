44b2a0d9c3755472f297ab360078875cfd1b3677
PiJei <piiijeiii@gmail.com>
2019-03-17 10:30:07 +0000

    added basic information of a B-tree node

--- Source/DataStructures/Trees/BTreeNode.cs
+++ Source/DataStructures/Trees/BTreeNode.cs
@@ -17,13 +17,84 @@
  * along with CSFundamentals.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace CSFundamentals.DataStructures.Trees
 {
-    class BTreeNode
+    /// <summary>
+    /// Implements a B-Tree node. A B-tree node is an ordered sequence of K keys, and K+1 children.
+    /// </summary>
+    /// <typeparam name="T1">Is the type of the keys in the tree. </typeparam>
+    /// <typeparam name="T2">Is the type of the values in the tree. </typeparam>
+    public class BTreeNode<T1, T2>
     {
+        /// <summary>
+        /// Is the minimum number of keys in a B-tree internal/leaf node. (Notice that a root has no lower bound on the number of keys. Intuitively when the tree is just being built it might start with 1, and grow afterwards.)
+        /// </summary>
+        public int MinKeys { get; private set; }
+
+        /// <summary>
+        /// Is the maximum number of keys in a B-tree internal/leaf/root node. This is often 2 times the MinKeys.
+        /// </summary>
+        public int MaxKeys { get; private set; }
+
+        /// <summary>
+        /// Is the minimum number of branches/children a B-tree internal node can have. 
+        /// </summary>
+        public int MinBranchingDegree { get; private set; }
+
+        /// <summary>
+        /// Is the maximum number of branches/children a B-tree internal or root node can have. Leaf nodes contain 0 children. 
+        /// </summary>
+        public int MaxBranchingDegree { get; private set; }
+
+        /// <summary>
+        /// Is the list of key-value pairs of the current node. 
+        /// </summary>
+        public List<KeyValuePair<T1, T2>> KeyValues { get; set; }
+
+        /// <summary>
+        /// Is the list of the children of this node. At any moment in time, (Children.Count = KeyValues.Count - 1)
+        /// </summary>
+        public List<BTreeNode<T1, T2>> Children = new List<BTreeNode<T1, T2>>();
+
+        /// <summary>
+        /// Is the parent of the current node.
+        /// </summary>
+        public BTreeNode<T1, T2> Parent = null;
+
+        public BTreeNode(int minKeys)
+        {
+            MinKeys = minKeys;
+            MaxKeys = 2 * minKeys;
+            MinBranchingDegree = minKeys + 1;
+            MaxBranchingDegree = MaxKeys + 1;
+        }
+
+        /// <summary>
+        /// Checks whether the current node is leaf. A node is leaf if it has no children. 
+        /// </summary>
+        /// <returns>True if the current node is leaf, and false otherwise. </returns>
+        public bool IsLeaf()
+        {
+            if (Children.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks whether the current node is root. A node is root if it has no parent.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsRoot()
+        {
+            if (Parent == null)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
