7d647d0bb4d960c8c2248e3b2b2216f2e21bce3d
PiJei <piiijeiii@gmail.com>
2019-03-17 11:24:07 +0000

    added place holders for linked lists

---
+++ Source/DataStructures/LinkedLists/bIDirectionalLinkedList.cs
@@ -0,0 +1,58 @@
+﻿/* 
+ * Copyright (c) 2019 (PiJei) 
+ * 
+ * This file is part of CSFundamentalAlgorithms project.
+ *
+ * CSFundamentalAlgorithms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CSFundamentalAlgorithms is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CSFundamentals.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+//TODO: In all operations keep head and tail updated, ... 
+
+// TODO: Have a sorted linked list also implemented 
+namespace CSFundamentals.DataStructures.LinkedLists
+{
+    public class BiDirectionalLinkedList<T>
+    {
+        public BiDirectionalLinkedListNode<T> Head = null;
+        public BiDirectionalLinkedListNode<T> Tail = null;
+
+        public void InsertAfter()
+        {
+            // todo
+        }
+
+        public void InsertBefore()
+        {
+            // todo
+        }
+
+        public void Append()
+        {
+            // todo
+        }
+
+        public BiDirectionalLinkedListNode<T> Search(T value)
+        {
+            throw new NotImplementedException();
+            // todo
+        }
+
+        public void Delete()
+        {
+            // todo
+        }
+    }
+}
