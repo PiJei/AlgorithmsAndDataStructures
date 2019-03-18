96717a465e93504393c25f1e6494f497c35547c3
PiJei <piiijeiii@gmail.com>
2019-03-18 09:11:17 +0000

    added code for the delete operation in singly linked list

--- Source/DataStructures/LinkedLists/SinglyLinkedList.cs
+++ Source/DataStructures/LinkedLists/SinglyLinkedList.cs
@@ -26,12 +26,45 @@ namespace CSFundamentals.DataStructures.LinkedLists
     {
         public override bool Delete(T1 value)
         {
-            throw new NotImplementedException();
+            SinglyLinkedNode<T1> previousNode = null;
+            SinglyLinkedNode<T1> currentNode = Head;
+            while (currentNode != null)
+            {
+                if (currentNode.Value.CompareTo(value) == 0)
+                {
+                    if (previousNode == null) /*Means we are deleting the head. */
+                    {
+                        Head = currentNode.Next;
+                        return true;
+                    }
+                    else
+                    {
+                        previousNode.Next = currentNode.Next;
+                        return true;
+                    }
+                }
+                else
+                {
+                    previousNode = currentNode;
+                    currentNode = currentNode.Next;
+                }
+            }
+            return false;
         }
 
+        /// <summary>
+        /// Inserts a new node in the list. Insert in a singly linked list is the fastest when treated as a prepend, meaning adding to the beginning of the list. 
+        /// </summary>
+        /// <param name="newValue">Is the value of the new node in the list.</param>
+        /// <returns>True in case of success.</returns>
         public override bool Insert(T1 newValue)
         {
-            throw new NotImplementedException();
+            SinglyLinkedNode<T1> newNode = new SinglyLinkedNode<T1>(newValue)
+            {
+                Next = Head
+            };
+            Head = newNode;
+            return true;
         }
     }
 }
