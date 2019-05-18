f996a7bccf7cc2009b51142704cacba625a44a52
PiJei <piiijeiii@gmail.com>
2019-05-18 09:20:47 +0100

    added comments for binary search tests finding dictinct duplicate and missing elements]

--- Tests/Algorithms/Search/BinarySearchTests.cs
+++ Tests/Algorithms/Search/BinarySearchTests.cs
@@ -31,7 +31,7 @@ namespace CSFundamentalsTests.Algorithms.Search
     {
         /// <summary>
         /// Tests the correctness of binary search algorithm on an array with distinct elements. 
-        /// To visualize how Binary Search is done over <see cref="SearchTests.List"/> see <img src = "../Images/Search/BinarySearch.png"/>.
+        /// To visualize step by step how Binary Search finds a distinct element (int value of 3) in <see cref="SearchTests.List"/> see <img src = "../Images/Search/BinarySearch.png"/>.
         /// </summary>
         [TestMethod]
         public void Search_DistinctElements()
@@ -41,6 +41,7 @@ namespace CSFundamentalsTests.Algorithms.Search
 
         /// <summary>
         /// Tests the correctness of binary search algorithm on an array with duplicate elements. 
+        /// To visualize step by step how Binary Search finds a duplicate element (int value of 90) in <see cref="SearchTests.List"/> see <img src = "../Images/Search/BinarySearch.png"/>.
         /// </summary>
         [TestMethod]
         public void Search_DuplicateElements()
@@ -50,6 +51,7 @@ namespace CSFundamentalsTests.Algorithms.Search
 
         /// <summary>
         /// Tests the correctness of binary search algorithm when the key does not exist in the array. 
+        /// To visualize step by step how Binary Search terminates without finding a missing element (int value of 15) in <see cref="SearchTests.List"/> see <img src = "../Images/Search/BinarySearch.png"/>.
         /// </summary>
         [TestMethod]
         public void Search_NonExistingElements()
